610a1d32e980e04b26ca12ddfec66ace8997bb9d
Sujal  Deoda <sujaldeoda@Sujals-MacBook-Pro.local>
2023-10-29 09:35:30 +0530

    tuple

--- website/app.py
+++ website/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-from flask import redirect
+from flask import Flask, render_template, request, redirect
 import sqlite3
+import numpy as np
 
 app = Flask(__name__)
 
@@ -40,7 +40,6 @@ cursor.execute('''
 conn.commit()
 conn.close()
 
-
 conn = sqlite3.connect('game.db')
 cursor = conn.cursor()
 
@@ -49,8 +48,8 @@ cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS games (
         email TEXT PRIMARY KEY,
-        Achievements TEXT
-        Time_Taken INT
+        Achievements TEXT,
+        Time_Taken INT,
         Score INT
     )
 ''')
@@ -59,45 +58,61 @@ conn.close()
 
 current_question = 0
 
+# Create a dictionary to store scores for each category
+OCEAN_scores = {
+    'Openness to Experience': 0,
+    'Conscientiousness': 0,
+    'Extroversion': 0,
+    'Agreeableness': 0,
+    'Neuroticism': 0,
+}
+
+OCEAN_normalise = {
+    'Openness to Experience': 0,
+    'Conscientiousness': 0,
+    'Extroversion': 0,
+    'Agreeableness': 0,
+    'Neuroticism': 0,
+}
 
 questions = [
-    # Openness to Experience
-    {'id': 'question1', 'question': 'I enjoy trying new things.', 'category': 'Openness to Experience'},
-    {'id': 'question2', 'question': 'I am intellectually curious.', 'category': 'Openness to Experience'},
-    {'id': 'question3', 'question': 'I have a vivid imagination.', 'category': 'Openness to Experience'},
-    {'id': 'question4', 'question': 'I appreciate art and beauty.', 'category': 'Openness to Experience'},
-    {'id': 'question5', 'question': 'I like to explore new ideas.', 'category': 'Openness to Experience'},
-    {'id': 'question6', 'question': 'I like to meet new people and go to gatherings.', 'category': 'Openness to Experience'},
+    # Openness to experience
+    {'id': 'question1', 'question': 'I enjoy trying new things.', 'categories': [['Openness to Experience', 3], ['Agreeableness', 1],['Neuroticism',1]]},
+    {'id': 'question2', 'question': 'I am intellectually curious.', 'categories': [['Openness to Experience', 3],['Conscientiousness',1],['Neuroticism',1]]},
+    # {'id': 'question3', 'question': 'I have a vivid imagination.', 'categories': [['Openness to Experience', 3]]},
+    # {'id': 'question4', 'question': 'I appreciate art and beauty.', 'categories': [['Openness to Experience',3]]},
+    # {'id': 'question5', 'question': 'I like to explore new ideas.', 'categories': [['Openness to Experience',3]]},
+    # {'id': 'question6', 'question': 'I like to meet new people and go to gatherings.', 'categories': [['Openness to Experience',3]]},
     
     # Conscientiousness
-    {'id': 'question7', 'question': 'I am organized and tidy.', 'category': 'Conscientiousness'},
-    {'id': 'question8', 'question': 'I pay attention to detail.', 'category': 'Conscientiousness'},
-    {'id': 'question9', 'question': 'I am responsible and reliable.', 'category': 'Conscientiousness'},
-    {'id': 'question10', 'question': 'I follow a strict schedule.', 'category': 'Conscientiousness'},
-    {'id': 'question11', 'question': 'I am diligent in my work.', 'category': 'Conscientiousness'},
+    {'id': 'question7', 'question': 'I am organized and tidy.', 'categories': [['Conscientiousness',3],['Neuroticism',1]]},
+    {'id': 'question8', 'question': 'I pay attention to detail.', 'categories': [['Conscientiousness',3]]},
+    # {'id': 'question9', 'question': 'I am responsible and reliable.', 'categories': [['Conscientiousness',3]]},
+    # {'id': 'question10', 'question': 'I follow a strict schedule.', 'categories': [['Conscientiousness',3]]},
+    # {'id': 'question11', 'question': 'I am diligent in my work.', 'categories': [['Conscientiousness',3]]},
     
     # Extroversion
-    {'id': 'question12', 'question': 'I enjoy social gatherings and parties.', 'category': 'Extroversion'},
-    {'id': 'question13', 'question': 'I am talkative and outgoing.', 'category': 'Extroversion'},
-    {'id': 'question14', 'question': 'I feel comfortable in social situations.', 'category': 'Extroversion'},
-    {'id': 'question15', 'question': 'I am energized by being around people.', 'category': 'Extroversion'},
-    {'id': 'question16', 'question': 'I have a wide circle of friends.', 'category': 'Extroversion'},
-    {'id': 'question17', 'question': 'I like to meet new people and go to gatherings.', 'category': 'Extroversion'},
+    # {'id': 'question12', 'question': 'I enjoy social gatherings and parties.', 'categories': [['Extroversion',3]]},
+    # {'id': 'question13', 'question': 'I am talkative and outgoing.', 'categories': [['Extroversion',3]]},
+    # {'id': 'question14', 'question': 'I feel comfortable in social situations.', 'categories': [['Extroversion',3]]},
+    # {'id': 'question15', 'question': 'I am energized by being around people.', 'categories': [['Extroversion',3]]},
+    {'id': 'question16', 'question': 'I have a wide circle of friends.', 'categories': [['Extroversion',3]]},
+    {'id': 'question17', 'question': 'I like to meet new people and go to gatherings.', 'categories': [['Extroversion',3],['Openness to Experience',3]]},
 
     # Agreeableness
-    {'id': 'question18', 'question': 'I am considerate and compassionate.', 'category': 'Agreeableness'},
-    {'id': 'question19', 'question': 'I avoid conflicts and arguments.', 'category': 'Agreeableness'},
-    {'id': 'question20', 'question': 'I am easy to get along with.', 'category': 'Agreeableness'},
-    {'id': 'question21', 'question': 'I am sympathetic to others\' feelings.', 'category': 'Agreeableness'},
-    {'id': 'question22', 'question': 'I often go out of my way to help others or resolve conflicts peacefully.', 'category': 'Agreeableness'},
-    {'id': 'question23', 'question': 'It is important for me to maintain harmonious relationships with those around me.', 'category': 'Agreeableness'},
+    {'id': 'question18', 'question': 'I am considerate and compassionate.', 'categories': [['Agreeableness',3]]},
+    {'id': 'question19', 'question': 'I avoid conflicts and arguments.', 'categories': [['Agreeableness',3]]},
+    # {'id': 'question20', 'question': 'I am easy to get along with.', 'categories': [['Agreeableness',3]]},
+    # {'id': 'question21', 'question': 'I am sympathetic to others\' feelings.', 'categories': [['Agreeableness',3]]},
+    # {'id': 'question22', 'question': 'I often go out of my way to help others or resolve conflicts peacefully.', 'categories': [['Agreeableness',3]]},
+    # {'id': 'question23', 'question': 'It is important for me to maintain harmonious relationships with those around me.', 'categories': [['Agreeableness',3]]},
 
     # Neuroticism
-    {'id': 'question24', 'question': 'I often feel anxious or nervous.', 'category': 'Neuroticism'},
-    {'id': 'question25', 'question': 'I am easily upset or stressed.', 'category': 'Neuroticism'},
-    {'id': 'question26', 'question': 'I worry about things.', 'category': 'Neuroticism'},
-    {'id': 'question27', 'question': 'I often feel down or depressed.', 'category': 'Neuroticism'},
-    {'id': 'question28', 'question': 'I am emotionally sensitive.', 'category': 'Neuroticism'}
+    {'id': 'question24', 'question': 'I often feel anxious or nervous.', 'categories': [['Neuroticism',3]]},
+    {'id': 'question25', 'question': 'I am easily upset or stressed.', 'categories': [['Neuroticism',3]]},
+    # {'id': 'question26', 'question': 'I worry about things.', 'categories': [['Neuroticism',3]]},
+    # {'id': 'question27', 'question': 'I often feel down or depressed.', 'categories': [['Neuroticism',3]]},
+    # {'id': 'question28', 'question': 'I am emotionally sensitive.', 'categories': [['Neuroticism',3]]}
 ]
 
 @app.route('/')
@@ -129,35 +144,50 @@ def submit():
         global question_id
         question_id = questions[current_question]['id']
 
-        return render_template('question.html', question_no=current_question+1, question=question, question_id=question_id)
+        return render_template('question.html', question_no=current_question + 1, question=question, question_id=question_id)
 
 @app.route('/Done', methods=['POST'])
 def Done():
     if request.method == 'POST':
         global current_question
         global email
-        global question_id 
-        answer = request.form['answer']
-        question_cat=questions[current_question]['category']
+        global question_id
+        answer = int(request.form['answer'])
+        categories = questions[current_question]['categories']
+
+        # Update scores for each category based on their weights
+        for category, weight in categories:
+            OCEAN_scores[category] += (weight * (answer/7))
+            OCEAN_normalise[category] += weight
+        
+            
+
         # Establish a connection to the database
         conn = sqlite3.connect('questionnaire.db')
         cursor = conn.cursor()
 
         # Insert the questionnaire answer along with the question ID into the database
-        cursor.execute('''
-            INSERT INTO user_responses (email, question_id, category, answer)
-            VALUES (?, ?, ?, ?)
-        ''', (email, question_id, question_cat, answer))
+        for category, _ in categories:
+            cursor.execute('''
+                INSERT INTO user_responses (email, question_id, category, answer)
+                VALUES (?, ?, ?, ?)
+            ''', (email, question_id, category, answer))
 
         conn.commit()
         conn.close()
 
         current_question += 1  # Move to the next question
+        current_question += 1  # Move to the next question
         if current_question < len(questions):
             question = questions[current_question]['question']
             question_id = questions[current_question]['id']
-            return render_template('question.html', question_no=current_question+1, question=question, question_id=question_id)
+            return render_template('question.html', question_no=current_question + 1, question=question, question_id=question_id)
         else:
+            # Normalize scores based on OCEAN_normalise values
+            for category, weight in OCEAN_scores.items():
+                if OCEAN_normalise[category] > 0:
+                    OCEAN_scores[category] /= OCEAN_normalise[category]
+            print(OCEAN_scores)
             return render_template('game.html')
 
 @app.route('/game', methods=['POST'])
@@ -180,3 +210,4 @@ def game():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
